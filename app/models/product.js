a1663fdea36f6cc0ed033692e64a9c88c011964b
Trevor Abram <trevor@fideagency.com>
2019-06-11 00:50:03 -0500

    Added models, serializer, and adapter for product data

---
+++ app/models/product.js
@@ -0,0 +1,11 @@
+import DS from 'ember-data';
+
+
+export default DS.Model.extend({
+
+    base_currency: DS.attr('string'),
+    quote_currency: DS.attr('string'),
+    base_min_size: DS.attr('number'),
+    base_max_size: DS.attr('number'),
+    quote_increment: DS.attr('number'),
+});
